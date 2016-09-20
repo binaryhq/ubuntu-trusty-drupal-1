c231aa1e9725f1d5e90898cd67fc08c3b69d0b5c
ningappa <ningappa@users.noreply.github.com>
2016-09-20 17:41:53 +0530

    Update create_mysql_admin_user.sh

--- create_mysql_admin_user.sh
+++ create_mysql_admin_user.sh
@@ -41,7 +41,28 @@ rm wordpress.sql
 # You can create a /mysql-setup.sh file to intialized the DB
 #if [ -f /install-drupal-standard.sh ] ; then
     echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!installing Drupal !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-  /install-drupal-standard.sh 
+    
+cd /var/www/html && drupal site:install standard \
+	--langcode en \
+	--site-name="Drupal 8" \
+	--db-type='mysql' \
+	--db-host="localhost" \
+	--db-port=3306 \
+	--db-user=${MYSQL_USER:-'admin'} \
+	--db-pass=${MYSQL_PASS:-'admin'} \
+	--db-name=${MYSQL_DBNAME:-'admin'} \
+	--db-prefix="drupal_" \
+	--site-mail=${USER_EMAIL:-'support@'$VIRTUAL_HOST} \
+	--account-name=${WP_USER:-'admin'} \
+	--account-mail=${USER_EMAIL:-'support@'$VIRTUAL_HOST} \
+	--account-pass=${WP_PASS:-'password'} && \
+    drupal check && \
+	drupal module:download admin_toolbar --latest && \ 
+	drupal module:install admin_toolbar --latest && \
+	drupal module:download devel --latest && \ 
+    drupal module:install devel --latest
+
+#  /install-drupal-standard.sh 
 #fi
 
 echo "=> Done!"
